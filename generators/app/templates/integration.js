35b574dd70debf20e1a0f16abb1fa136a009e91f
Nathan Jenan <nathan.jenan@polarity.io>
2018-02-28 07:09:26 -0700

    Adding tag template and better config options around https

--- generators/app/templates/integration.js
+++ generators/app/templates/integration.js
@@ -2,6 +2,11 @@ let async = require('async');
 let config = require('./config/config');
 
 let Logger;
+let requestOptions = {};
+
+function getRequestOptions() {
+    return JSON.parse(JSON.stringify(requestOptions));
+}
 
 function doLookup(entities, options, callback) {
     callback(null, null);
@@ -9,15 +14,34 @@ function doLookup(entities, options, callback) {
 
 function startup(logger) {
     Logger = logger;
+
+    if (typeof config.request.cert === 'string' && config.request.cert.length > 0) {
+        requestOptions.cert = fs.readFileSync(config.request.cert);
+    }
+
+    if (typeof config.request.key === 'string' && config.request.key.length > 0) {
+        requestOptions.key = fs.readFileSync(config.request.key);
+    }
+
+    if (typeof config.request.passphrase === 'string' && config.request.passphrase.length > 0) {
+        requestOptions.passphrase = config.request.passphrase;
+    }
+
+    if (typeof config.request.ca === 'string' && config.request.ca.length > 0) {
+        requestOptions.ca = fs.readFileSync(config.request.ca);
+    }
+
+    if (typeof config.request.proxy === 'string' && config.request.proxy.length > 0) {
+        requestOptions.proxy = config.request.proxy;
+    }
+
+    if (typeof config.request.rejectUnauthorized === 'boolean') {
+        requestOptions.rejectUnauthorized = config.request.rejectUnauthorized;
+    }
 }
 
 function validateOptions(options, callback) {
-    callback(null, [
-        {
-            key: '',
-            message: 'No user option validation implemented'
-        }
-    ]);
+    callback(null, null);
 }
 
 module.exports = {
